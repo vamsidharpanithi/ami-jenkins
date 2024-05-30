15f1f5c8191ab0bb168392202952c56fa93d53d7
Mihir Adelkar <adelkar.m@northeastern.edu>
2024-05-30 16:12:38 -0400

    added certbot to nginx

--- packer/install-nginx.sh
+++ packer/install-nginx.sh
@@ -5,4 +5,5 @@ sudo apt-get upgrade
 sudo apt-get install nginx -y
 sudo systemctl start nginx
 sudo systemctl enable nginx
+sudo apt-get install certbot python3-certbot-nginx -y
 sudo systemctl status nginx
