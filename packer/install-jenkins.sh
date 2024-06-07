e56781e883e6ada40f8aaed1167ebba85d0786d5
Vamsidhar858 <145138793+Vamsidhar858@users.noreply.github.com>
2024-06-07 02:24:51 -0400

    Installing Docker

--- packer/install-jenkins.sh
+++ packer/install-jenkins.sh
@@ -72,9 +72,22 @@ sudo systemctl status jenkins
 echo "================================="
 echo "Installing Docker"
 echo "================================="
-sudo apt install docker.io -y
-sudo systemctl start docker
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 sudo systemctl enable docker
+sudo systemctl start docker
 sudo usermod -aG docker $USER
 sudo usermod -aG docker jenkins
 
