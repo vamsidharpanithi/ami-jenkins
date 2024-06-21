92b15a81cb3c98ac9c6bd40f57b56302139afa63
Vamsidhar858 <145138793+Vamsidhar858@users.noreply.github.com>
2024-06-21 00:37:36 -0400

    fix: updated DSL for AWS Infra

--- packer/job-dsl.groovy
+++ packer/job-dsl.groovy
@@ -98,3 +98,28 @@ pipelineJob('DB-CVE') {
         githubPush()
     }
 }
+pipelineJob('Aws-Infras') {
+    displayName('Aws Infra')
+    description('Pipeline to build Aws Infra')
+    logRotator {
+        daysToKeep(30)
+        numToKeep(10)
+    }
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url('https://github.com/cyse7125-su24-team04/infra-aws.git')
+                        credentials('github_token')
+                    }
+                    branch('*/main')
+                }
+                scriptPath('Jenkinsfile.semantic')
+            }
+        }
+    }
+    triggers{
+        githubPush()
+    }
+}
